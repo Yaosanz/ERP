447b3b45ea2437e09ceea833d4a77e0449ac0eee
Sandy Budi Wirawan <sandybudiwirawan@gmail.com>
2024-11-24 19:30:07 +0700

    feat(admin/vendor): added vendor and fix sequence migration

---
+++ app/Models/Vendor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vendor extends Model
+{
+    use HasFactory;
+    // protected $table = 'vendors';
+    protected $fillable = [
+        'name',
+        'address',
+        'number_phone',
+        'email',
+        'province_id',
+        'city_id',
+        'country_id',
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Countries::class, 'country_id');
+    }
+
+    public function province()
+    {
+        return $this->belongsTo(Provinces::class, 'province_id');
+    }
+
+
+    public function city()
+    {
+        return $this->belongsTo(Cities::class, 'city_id');
+    }
+}
