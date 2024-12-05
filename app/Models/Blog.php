884ed555fd414ba9be2338d633bfbe0f3090e9e3
Sandy Budi Wirawan <sandybudiwirawan@gmail.com>
2024-12-05 16:51:04 +0700

    feat(admin/blogs):fix blogging

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Blog extends Model
 {
@@ -17,6 +18,16 @@ class Blog extends Model
         'published',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($blog) {
+            if (empty($blog->slug)) {
+                $blog->slug = Str::slug($blog->title);
+            }
+        });
+    }
     protected $casts = [
         'tags' => 'array',
     ];
