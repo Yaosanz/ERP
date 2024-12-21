419a680c9f2caa9a38a4aecd3cca80c2f9980d40
Sandy Budi Wirawan <sandybudiwirawan@gmail.com>
2024-12-21 13:29:47 +0700

    fix:(blogs): add req handling http req file uploads

--- app/Filament/Resources/BlogResource/Pages/CreateBlog.php
+++ app/Filament/Resources/BlogResource/Pages/CreateBlog.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\BlogResource\Pages;
 
 use App\Filament\Resources\BlogResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Storage;
 
 class CreateBlog extends CreateRecord
 {
@@ -12,5 +13,14 @@ class CreateBlog extends CreateRecord
     protected function getRedirectUrl(): string
     {
         return static::$resource::getUrl('index');
+    }
+
+    public function uploadThumbnail($file)
+    {
+        // Tentukan lokasi penyimpanan
+        $path = $file->store('public/blogs');
+        
+        // Dapatkan URL file yang di-upload
+        return Storage::url($path);
     }
 }
